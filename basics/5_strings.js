2b8b39659b9e6e1c37f3e774f29a62a40830e2be
Shubham Yadre <127323514+ShubhGithub07@users.noreply.github.com>
2024-01-17 18:43:44 +0000

    strings

---
+++ basics/5_strings.js
@@ -0,0 +1,29 @@
+const name = "Shubham"
+const repoCount = 70
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+let newName = new String('Shubh-am-Yadre') // new string forms a object
+// console.log(newName[4]);
+// console.log(newName.__proto__);
+
+// console.log(newName.length);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt(7));
+// console.log(newName.indexOf('b'));
+
+const newString = newName.substring(0, 7)
+// console.log(newString);
+
+const anotherString = newName.slice(-12, 5)
+// console.log(anotherString);
+
+const stringOne = "    Shubham          "
+// console.log(stringOne);
+// console.log(stringOne.trim());
+
+const url = "https://Shubh.com/Shubh%70Yadre"
+// console.log(url.replace('%70', '-'));
+// console.log(url.includes('Shubh'));
+
+console.log(newName.split('-'));
