7ec68305c6e1f942a677238ac41429cea737078f
Shubham Yadre <127323514+ShubhGithub07@users.noreply.github.com>
2024-01-18 06:06:33 +0000

    Arrays_function

--- basics/7_DateandTime.js
+++ basics/7_DateandTime.js
@@ -17,4 +17,31 @@ let newDate = new Date("01-23-2022")
 let myTimeStamp = Date.now()
 // console.log(myTimeStamp);
 // console.log(newDate.getTime());
-console.log(Math.floor(Date.now()/10000));
+// console.log(Math.floor(Date.now()/10000));
+
+
+// **************** Array function **************//
+
+const marvel = ["Ironman", "thor", "hulk"]
+const dc = ["supperman", "batman", "flash"]
+
+// let newHeros = marvel.concat(dc)
+// console.log(newHeros);
+
+const all_heros = [...marvel, ...dc] // use in industry
+// console.log(all_heros);
+
+const arr = [1, 2, 3, [4, 5], 6, [7, 8, [4, 5, 7]]] // Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth
+const realArr = arr.flat(Infinity)
+
+// console.log(realArr);
+
+// console.log(Array.isArray("Shubham"));
+// console.log(Array.from("Shubham"));
+console.log(Array.from({name: "Shubham"})); // when object is pass in array then it return's empty array, we should specify either with it's key or value
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // Returns a new array from a set of elements.
