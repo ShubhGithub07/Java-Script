2a4ce223cc9964b11580be1a6ec9116fc3cf87ad
Shubham Yadre <127323514+ShubhGithub07@users.noreply.github.com>
2024-01-26 12:11:12 +0000

    callstack

---
+++ 2.0_basics/5_callStack.js
@@ -0,0 +1,10 @@
+let val1 = 10
+let val2 = 5
+function addnum(n1, n2){
+    let total = n1 + n2
+    console.log(total)
+}
+
+let result1 = addnum(val1, val2)
+let result2 = addnum(4, 3)
+// console.log(addnum(4, 3))
