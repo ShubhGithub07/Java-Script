517598d0aba41b121f2723681044b61aa9bdae2a
Shubham Yadre <127323514+ShubhGithub07@users.noreply.github.com>
2024-01-26 12:59:17 +0000

    control flow

---
+++ 3.0_control_flow/one.js
@@ -0,0 +1,26 @@
+// ************************* Control flow ****************************** // 
+
+// if else
+// nested if else
+// logic operator: &&, ||, !
+// switch statement
+
+// falsy value 
+// false, 0, -0, null, undefined, BigInt 0n, "", NaN
+
+// truthy value 
+// "0", 'false', " ", [], {}, function(){}
+
+// Nullish coalescing operator (??): null undefined
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10
+
+// console.log(val1)
+
+
+// ternary operator
+// condition ? true : false
+
+let vadaPav = 15
+vadaPav >= 12 ? console.log("less than 12") : console.log("more than 12")
